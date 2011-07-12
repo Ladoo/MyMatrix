71be4a8d91f4367c3f3a5a3a757bfa285d6b0a07
Anton Babushkin <vegitto.ru@gmail.com>
2011-07-12 17:29:06 +1000

    extended the js wrapper code

--- chrome/content/matrixdevelopertoolbar.js
+++ chrome/content/matrixdevelopertoolbar.js
@@ -1,16 +1,51 @@
 var mdt = function () {
 	var prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
 	return {
-		init: function() {
-			gBrowser.addEventListener("load", function () {
-				//var autoRun = prefManager.getBoolPref("extensions.matrixdevelopertoolbar.autorun");
+		// general settings
+		settings: {
+			
+		},
+		
+		// about the current tab the user is browsing
+		// everything from the asset type
+		aboutTab: {
+			isMatrixBackend: false
+		},
+		
+		init: function(){
+			gBrowser.addEventListener("load", function(){
+				if (mdt.isMatrixBackend()) {
+					aboutTab.isMatrixBackend = true;
+					mdt.determineAssetType();
+				}
 			}, true);
 		},
 
-		bindButtons: function() {
+		determineAssetType: function(){
+			
 		},
 		
-		open: function(){
+		enhanceAsset: function(){
+			
+		},
+		
+		replaceWYSIWYG: function(){
+			
+		},
+		
+		highlightSyntax: function(){
+			
+		},
+		
+		isMatrixBackend: function(){
+			return (content.document.title.match(/(Squiz|MySource)\sMatrix.*Administration Interface/)) ? true : false;
+		},
+		
+		isMatrixSite: function(){
+			// Perform 2 checks
+			// 1. Is there a MySource / Squiz Matrix comment area in the <head>?
+			// 2. Do any of the web paths have /__data/ in them?
+			
 		}
 	};
 }();
